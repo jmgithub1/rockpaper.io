9041f591b5bf44e2e18907347d4d3e6df0c90755
James Mallen <james.mallentech@gmail.com>
2020-09-30 14:27:39 +0100

    File Creation

---
+++ app.js
@@ -0,0 +1,24 @@
+// a function that gathers the users moves into a variable
+let userMove = prompt ("Rock, Paper or Scissors?")
+
+function userChoice (userMove){
+    return userMove
+}
+alert(choice(userMove))
+
+
+
+
+
+
+
+// a function that randomly generates a computer move
+
+
+
+
+
+
+
+
+// a function that compares the two moves to see who has won the round.
